028b373f0a41ec5c60b980efd752d0066cd42f84
Laetitia LEJEUNE <llejeune@e3r1p9.42.fr>
2019-12-10 20:01:47 +0100

    tentative mvc signup

--- index.php
+++ index.php
@@ -1,21 +1,13 @@
 <?php
+require('sign_up.php');
 include('config/database.php');
 $conn = connexion();
-if (!isset($_POST['login']) && !isset($_POST['mail']) && !isset($_POST['password'])){
-
-    $sql = "INSERT INTO `user`(`login`, `mail`, `password`) VALUES ('" . $_POST['login'] . "', '" . $_POST['mail'] . "', '" . $_POST['password'] . "')";
-    $conn->query($sql);
+var_dump($_POST);
+if (isset($_POST) && isset($_POST['login']) && isset($_POST['mail']) && isset($_POST['password'])){
+    if ($_POST['login'] != "" && $_POST['mail'] != "" && $_POST['password'] != ""){
+        $sql = "INSERT INTO `user`(`login`, `mail`, `password`) VALUES ('{$_POST['login']}', '{$_POST['mail']}', '{$_POST['password']}')";
+        $conn->query($sql);
+        $conn = null;
+    }
 }
-$conn = null;
 ?>
-
-<?php include('header.php'); ?>
-<form name="signup" method="post" action="index.php">
-
-<input name="login" type="text" placeholder="Login" /></br>
-<input name="mail" type="email" placeholder="Mail address"/></br> 
-<input name="password" type="password" placeholder="Password" /></br>
-<input name="submit" type="submit" value="Sign up">
-
-</form>
-<?php include('footer.php'); ?>
