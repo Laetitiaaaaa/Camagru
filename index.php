7235f1beb6da3826bbf22f4ad012910c87669073
Laetitia LEJEUNE <llejeune@e3r1p9.42.fr>
2019-12-10 20:39:58 +0100

    organisation de fichiers

--- index.php
+++ index.php
@@ -1,13 +1,9 @@
 <?php
-require('sign_up.php');
-include('config/database.php');
-$conn = connexion();
-var_dump($_POST);
+require('models/sign_up.php');
+
 if (isset($_POST) && isset($_POST['login']) && isset($_POST['mail']) && isset($_POST['password'])){
-    if ($_POST['login'] != "" && $_POST['mail'] != "" && $_POST['password'] != ""){
-        $sql = "INSERT INTO `user`(`login`, `mail`, `password`) VALUES ('{$_POST['login']}', '{$_POST['mail']}', '{$_POST['password']}')";
-        $conn->query($sql);
-        $conn = null;
-    }
+    insert_user($_POST['login'], $_POST['mail'], $_POST['password']);
 }
+
+require('vues/sign_up.php');
 ?>
