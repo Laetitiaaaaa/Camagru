8501ea63e68b9d47d3bd55edc5b438c4f004f443
Laetitia LEJEUNE <llejeune@e3r5p17.42.fr>
2019-12-12 12:42:40 +0100

    forgot pass ok mail send

--- controler/forgot-pass.php
+++ controler/forgot-pass.php
@@ -3,7 +3,8 @@ require('../models/forgot-pass.php');
 
 if (isset($_POST) && isset($_POST['mail'])){
     if ($_POST['mail'] != ""){
-        
+        $mail = $_POST['mail'];
+        sendPass($mail);
     }
 }
 
--- models/forgot-pass.php
+++ models/forgot-pass.php
@@ -1,8 +1,61 @@
 <?php
 include('../config/database.php');
 
-function isMail(){}
+function isMail($mail){
+    $conn = connexion();
+    $sql = "SELECT `mail` FROM `user` WHERE `mail` = '{$mail}';";
+    $req = $conn->query($sql);
+    $data = $req->fetchAll(PDO::FETCH_ASSOC);
+    $conn = null;
+    if ($data){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
 
-function sendPass(){}
+function getUserwMail($mail){
+    $conn = connexion();
+    $sql = "SELECT * FROM `user` WHERE `mail` = '{$mail}';";
+    $req = $conn->query($sql);
+    $data = $req->fetchAll(PDO::FETCH_ASSOC);
+    return ($data[0]);
+}
+
+function insertNum($mail, $num){
+    $conn = connexion();
+    $sql = "UPDATE `user` SET `num` = '{$num}' WHERE `mail` = '{$mail}';";
+    $conn->query();
+    $conn = null;
+    var_dump("num user inserted");
+}
+
+function sendPass($mail){
+    if (isMail($mail) == false){
+        return false;
+    }
+    else{
+        $num = rand(0, 1000000);
+        insertNum($mail, $num);
+        $user = getUserwMail($mail);
+        $login = $user['login'];
+        $subject = "Forgotten password";
+        $headers = "MIME-Version: 1.0" . "\r\n";
+        $headers = "Content-type:text/html;charset=UTF-8" . "\r\n";
+        $headers .= "From: Camagru Team <no_reply@camagru.com>"."\r\n";
+        $message = "
+        <h1>No worries $login!</h1>
+        <p>To change your password, click <a href='http://localhost:8080/controler/change-passwd.php?log=$login&n=$num'>here</a>.</p>
+        ";
+        $chpass = mail($mail, $subject, $message, $headers);
+        if ($chpass == true){
+            var_dump("chpass envoyé");
+        }
+        else{
+            var_dump("pb mail chpass");
+        }
+    }
+}
 
 ?>
