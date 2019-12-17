29cffafd2e4d9b0902228fcf119c0e0396f98e12
Laetitia LEJEUNE <llejeune@e1r4p14.42.fr>
2019-12-17 17:55:03 +0100

    meilleure organisation code

--- views/home.php
+++ views/home.php
@@ -1,43 +1,55 @@
 <?php include('header.php');?>
 
 <div style="border:3px solid pink;">
+    
     <select name="filter" id="filter" onchange="changeFilter()" style="display:block;">
         <option value="dino">Dino</option>
         <option value="heart">Heart balloons</option>
         <option value="eve">Eve (Wall-E)</option>
         <option value="fox">Fox</option>
     </select>
+
     <div id="bluebox" style="display:inline-block; border:3px solid blue;width: 50%;">
+      
       <div id="divideo" style="border: 3px solid black; width:100%; position: relative;">
-        <!-- <video id="video" autoplay style="width:100%;"></video> -->
-        <!-- <img id="putfilter" src="../filters/dino.png" alt="dino" style="position:absolute; right:-1.5%; bottom:-3.9%; z-index: 2; width:52%;"> -->
       </div>
-    <!-- <canvas id="canvas" style="display:none;"></canvas> -->
 
-    <!-- <form enctype="multipart/form-data" method="post" action="/controler/home.php">
-
-    <input id="filterpic" type="text" name="filterpic" value="dino" style="display:none;">
-    <input id="filepic" type="text" name="picture" style="display:none;">
-
-    </form> -->
-      <!-- <input id="photoButton" type="submit" value="Take picture"> -->
     </div>
+    
     <div id="preview" style="border:3px solid grey; display: inline-block; vertical-align: top; text-align:center; width: 40%;">
     </div>
+
 </div>
 
 <script>
 
 var constraints = { audio: false, video: true };
 var filter = document.querySelector('#putfilter');
-// var video = document.querySelector('video');
-var canvas = document.querySelector('canvas');
-var photoButton = document.getElementById('photoButton');
 var select = document.getElementById('filter');
 var httpRequest = new XMLHttpRequest;
 
 navigator.mediaDevices.getUserMedia(constraints)
 .then(function(mediaStream) {
+  createElemVideo();
+  
+  var video = document.querySelector('video');
+  video.srcObject = mediaStream;
+  video.onloadedmetadata = function(e) {
+    video.play();
+  };
+
+  document.getElementById('photoButton').addEventListener('click', (ev) => {
+  var dataPic = takepicture();
+  var dataSel = select.value;
+  postData(dataPic, dataSel);
+  });
+})
+.catch(function(err) {
+  console.log(err.name + ": " + err.message);
+  
+  });
+
+function createElemVideo(){
   var divideo = document.getElementById('divideo');
   var bluebox = document.getElementById('bluebox');
 
@@ -61,24 +73,27 @@ navigator.mediaDevices.getUserMedia(constraints)
   photoButton.setAttribute('type', 'submit');
   photoButton.setAttribute('value', 'Take picture');
   bluebox.appendChild(photoButton);
+}
 
-  video.srcObject = mediaStream;
-  video.onloadedmetadata = function(e) {
-    video.play();
-  };
+function takepicture(){
+    var canvas = document.querySelector('canvas');
+    var video = document.querySelector('video');
 
-  document.getElementById('photoButton').addEventListener('click', (ev) => {
-  var dataPic = takepicture();
-  var dataSel = select.value;
-  // console.log(dataPic);
-  // console.log(dataSel);
-  postData(dataPic, dataSel);
-  });
-})
-.catch(function(err) {
-  console.log(err.name + ": " + err.message);
-  
-  });
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    canvas.getContext('2d').drawImage(video, 0, 0);
+    
+    var data = canvas.toDataURL();
+    return data;
+}
+
+function postData(dataPic, dataSel){
+    var formData = new FormData();
+    formData.append('picture', dataPic);
+    formData.append('filterpic', dataSel);
+    httpRequest.open('POST', '/controler/home.php');
+    httpRequest.send(formData);
+}
 
 function changeFilter() {
   var choice = document.getElementById('filter').value;
@@ -110,12 +125,13 @@ function display_picture(){
   if (httpRequest.readyState === XMLHttpRequest.DONE){
     if (httpRequest.status === 200){
       subject = httpRequest.response;
-      alert(subject);
       pattern = RegExp('\.\.\/gallery\/.*\.png');
       ans = subject.match(pattern);
+      
       if (ans != null){
         addImg(ans[0]);
       }
+    
     }
     else{
       alert('pb request');
@@ -134,26 +150,6 @@ function addImg(src){
   img.setAttribute('style', 'width:100%;');
   img.setAttribute('src', src);
   div.appendChild(img);
-}
-
-function postData(dataPic, dataSel){
-    var formData = new FormData();
-    formData.append('picture', dataPic);
-    formData.append('filterpic', dataSel);
-    httpRequest.open('POST', '/controler/home.php');
-    httpRequest.send(formData);
-}
-
-function takepicture(){
-    var canvas = document.querySelector('canvas');
-    var video = document.querySelector('video');
-
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
-    canvas.getContext('2d').drawImage(video, 0, 0);
-    
-    var data = canvas.toDataURL();
-    return data;
 }
 
 </script>
