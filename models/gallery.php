4c738e5fc724fb8e5fec0410b81a96755838f0e1
Laetitia LEJEUNE <llejeune@e3r5p17.42.fr>
2019-12-18 17:42:46 +0100

    affichage gallery plus recent au moins recent ok

--- models/gallery.php
+++ models/gallery.php
@@ -3,7 +3,7 @@ include('../config/database.php');
 
 function recoverFiles(){
     $conn = connexion();
-    $sql = "SELECT *, `gallery`.`id` AS 'id_gall' FROM `gallery` INNER JOIN `user` WHERE `user`.`id` = `gallery`.`id_user`;";
+    $sql = "SELECT *, `gallery`.`id` AS 'id_gall' FROM `gallery` INNER JOIN `user` WHERE `user`.`id` = `gallery`.`id_user` ORDER BY `gallery`.`id` DESC;";
     $req = $conn->query($sql);
     $conn = null;
     $data = $req->fetchAll(PDO::FETCH_ASSOC);
