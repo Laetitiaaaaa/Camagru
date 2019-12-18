f111e2f5af25768a1c95798402cddd98dd1d487f
Laetitia LEJEUNE <llejeune@e3r5p17.42.fr>
2019-12-18 17:37:39 +0100

    path gallery image ok

--- views/gallery.php
+++ views/gallery.php
@@ -2,7 +2,7 @@
 
 <?php foreach ($photo_tab as $photo){?>
 
-<div style="width:30%; display:inline-block;"><a href="/controler/photo.php?log=<?php echo $photo['login'] ?>&n=<?php echo $photo['id_gall'] ?>"><img src="../gallery/<?php echo $photo['path']?>" style="width:100%;"></a></div>
+<div style="width:30%; display:inline-block;"><a href="/controler/photo.php?log=<?php echo $photo['login'] ?>&n=<?php echo $photo['id_gall'] ?>"><img src="../gallery/newPictures/<?php echo $photo['path']?>" style="width:100%;"></a></div>
 
 <?php }?>
 
