381f0e0bc65039728e7e2e066cf156544c8ff24e
Laetitia LEJEUNE <llejeune@e3r4p18.42.fr>
2019-12-19 14:08:16 +0100

    onglet my account uniquement si connecte

--- views/header.php
+++ views/header.php
@@ -7,7 +7,7 @@
 <header>
 <a href="/controler/home.php">Camagru</a>
 <a href="http://localhost:8080/controler/gallery.php">Gallery</a>
-<a href="/controler/myAccount.php">My Account</a>
+<?php if ($_SESSION['logon'] == 1){?><a href="/controler/myAccount.php">My Account</a><?php } ?>
 <?php if ($_SESSION['logon'] == 1){?><a href="http://localhost:8080/controler/signOut.php">Sign Out</a><?php } ?>
 <?php if ($_SESSION['logon'] != 1){?><a href="http://localhost:8080/controler/signIn.php">Sign In</a><?php } ?>
 <?php if ($_SESSION['logon'] != 1){?><a href="http://localhost:8080/controler/signUp.php">Sign Up</a><?php } ?>
