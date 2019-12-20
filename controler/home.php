b65c3b48ce62763dddbdc6f299863adb8d511e7e
Laetitia LEJEUNE <llejeune@e3r4p18.42.fr>
2019-12-20 15:25:13 +0100

    on commence la mise au propre du home avec method = POST

--- controler/home.php
+++ controler/home.php
@@ -1,65 +1,55 @@
 <?php
 require($root . '/models/home.php');
 
-if (isset($_SESSION) && isset($_SESSION['logon']) && isset($_SESSION['login'])){
-    if ($_SESSION['logon'] == 1){
-        if (isset($_POST) && (isset($_POST['picture']) && isset($_POST['filterpic']))){
-            if ($_POST['picture'] != "" && $_POST['filterpic'] != ""){
-                $img = $_POST['picture'];
-                $filter = $_POST['filterpic'];
-                $ans = true;
-                $login = $_SESSION['login'];
-                if (isPath($img) == false){
-                    $tab = save_cam($img, $login);
-                    $ans = $tab[1];
-                    $filename = $tab[0];
-                }
-                else{
-                    $filename = $img;
-                }
-                if ($ans != false){
-                    if ($filter == 'dino'){
-                        $filtername = 'filters/dino.png';
-                    }
-                    else if ($filter == 'heart'){
-                        $filtername = 'filters/coeurs.png';
-                    }
-                    else if ($filter == 'eve'){
-                        $filtername = 'filters/eveuh.png';
-                    }
-                    else if ($filter == 'fox'){
-                        $filtername = 'filters/fox.png';
-                    }
-                    put_image($_SESSION['login'], $filename, $filtername);
-                }
-            }
+if($method == 'POST'){
+    if ($_POST['picture'] != "" && $_POST['filterpic'] != ""){
+        $img = $_POST['picture'];
+        $filter = $_POST['filterpic'];
+        $ans = true;
+        $login = $_SESSION['login'];
+        if (isPath($img) == false){
+            $tab = save_cam($img, $login);
+            $ans = $tab[1];
+            $filename = $tab[0];
         }
-        else if (isset($_FILES) && $_FILES['download'] != ""){
-            $img = $_FILES['download'];
-            if ($img['type'] != 'image/png'){
-                var_dump('mauvais format');
-            }
-            else{
-                $num = rand(0, 100000);
-                $tmp_name = $img['tmp_name'];
-                $name = $_SESSION['login'] . '_' . $num . '.png';
-                if (fileExists($name) != 0){
-                    $num = rand(0, 500);
-                    $new = '_' . $num . '.png';
-                    $name = str_replace('.png', $new, $name);
-                }
-                $filename = 'gallery/uploadedPictures/' . $name;
-                move_uploaded_file($tmp_name, $filename);
-            }
+        else{
+            $filename = $img;
         }
-        require($root . '/views/home.php');
+        if ($ans != false){
+            if ($filter == 'dino'){
+                $filtername = 'filters/dino.png';
+            }
+            else if ($filter == 'heart'){
+                $filtername = 'filters/coeurs.png';
+            }
+            else if ($filter == 'eve'){
+                $filtername = 'filters/eveuh.png';
+            }
+            else if ($filter == 'fox'){
+                $filtername = 'filters/fox.png';
+            }
+            put_image($_SESSION['login'], $filename, $filtername);
+        }
     }
-    else{
-        require($root . '/views/notLogon.php');
+    else if (isset($_FILES) && $_FILES['download'] != ""){
+        $img = $_FILES['download'];
+        if ($img['type'] != 'image/png'){
+            var_dump('mauvais format');
+        }
+        else{
+            $num = rand(0, 100000);
+            $tmp_name = $img['tmp_name'];
+            $name = $_SESSION['login'] . '_' . $num . '.png';
+            if (fileExists($name) != 0){
+                $num = rand(0, 500);
+                $new = '_' . $num . '.png';
+                $name = str_replace('.png', $new, $name);
+            }
+            $filename = 'gallery/uploadedPictures/' . $name;
+            move_uploaded_file($tmp_name, $filename);
+        }
     }
 }
-else{
-    require($root . '/views/notLogon.php');    
-}
+require($root . '/views/home.php');
 
 ?>
