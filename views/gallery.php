fecf041f0ab82fe8d207b9c1b1a498b8c6323dcb
Laetitia LEJEUNE <llejeune@e1r4p12.42.fr>
2019-12-21 17:37:34 +0100

    affichage galery okkkkkk

--- views/gallery.php
+++ views/gallery.php
@@ -1,16 +1,34 @@
 <?php include('header.php');?>
 
-<div style="text-align: center; margin-top: 3%;">
-
-<?php foreach ($pageGallery as $photo){?>
-
-<div style="width:30%; display:inline-block;">
-    <a href="/photo?log=<?php echo $photo['login'] ?>&n=<?php echo $photo['id_gall'] ?>"><img src="/gallery/newPictures/<?php echo $photo['path']?>" style="width:100%;"></a>
+<div class="content has-text-centered">
+    <h1>Gallery</h1>
 </div>
 
-<?php }?>
-
+<div class="columns">
+    <div class="column is-1"></div>
+    <div class="column is-10">
+        <div class="columns is-multiline is-centered">
+        <?php foreach ($pageGallery as $photo){?>
+            <div class="column is-one-third">
+                <div class="card">
+                    <div class="card-image">
+                        <figure class="image is-4by3">
+                            <a href="/photo?log=<?php echo $photo['login'] ?>&n=<?php echo $photo['id_gall'] ?>"><img src="/gallery/newPictures/<?php echo $photo['path']?>" style="width:100%;"></a>
+                        </figure>
+                    </div>
+                </div>
+            </div>
+        <?php }?>
+        </div>
+    </div>
+    <div class="column is-1"></div>
 </div>
+
+
+
+
+
+
 
 <?php 
 if ($nbPage == 0){
@@ -19,17 +37,19 @@ if ($nbPage == 0){
 ?>
 <div style="width:30%;" class="container">
     <nav class="pagination is-centered" role="navigation" aria-label="pagination">
-    
+
+    <ul class="pagination-list">
+
     <?php if (isset($previous)){ ?>
-    <a class="pagination-previous" href="/gallery?page=<?php echo $previous ?>">Previous</a>
+    <li><a class="pagination-previous" href="/gallery?page=<?php echo $previous ?>">Previous</a></li>
     <?php } ?>
-    
+
+    <li><?php echo $currentPage . '/' . $nbPage . ' '; ?><li>
+
     <?php if (isset($next)){ ?>
-    <a class="pagination-next" href="/gallery?page=<?php echo $next ?>">Next page</a>
+    <li><a class="pagination-next" href="/gallery?page=<?php echo $next ?>">Next page</a><li>
     <?php } ?>
   
-    <ul class="pagination-list">
-    <?php echo $currentPage . '/' . $nbPage . ' '; ?>
     </ul>
     
     </nav>
