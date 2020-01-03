9ff7dff992efc914de06c5307de8d614d8ed3ef0
Laetitia LEJEUNE <llejeune@e3r5p17.42.fr>
2020-01-03 17:40:55 +0100

    photo try catch ok

--- controler/photo.php
+++ controler/photo.php
@@ -7,7 +7,7 @@ if ($method == 'GET'){
         $id_photo = $_GET['n'];
         $photo = photoExists($id_user['id'], $id_photo);
 
-        if ($photo == false){
+        if ($photo === false || $photo === 'error' || $id_user === 'error'){
             $_SESSION['messInfo'] = 'Wrong path.';
             header('Location: ' . $fullDomain . '/gallery');
             exit;
@@ -16,11 +16,14 @@ if ($method == 'GET'){
         $file_img = getPhoto($id_user['id'], $id_photo)['path'];
         $nbLike = countLike($id_user['id'], $id_photo);
         $comments = getComment($file_img);
+        if ($file_img === 'error' ||  $nbLike === 'error' || $comments === 'error'){
+            $_SESSION['messInfo'] = 'Error.';
+        }
         require($root . '/views/photo.php');
     }
 }
 
-if ($method == 'POST')
+else if ($method == 'POST')
 {
     if (!empty($_POST['namePhoto']) && !empty($_POST['logUser']) && !empty($_POST['idPhoto'])){
         $file_img = $_POST['namePhoto'];
@@ -28,32 +31,48 @@ if ($method == 'POST')
         $idPhoto = $_POST['idPhoto'];
 
         if (!empty($_POST['like'])){
-            likePhoto($file_img);
+            $like = likePhoto($file_img);
             $nbLike = countLike($idUser['id'], $idPhoto);
+            if ($like === 'error' || $nbLike === 'error'){
+                $_SESSION['messInfo'] = 'Error.';
+            }
         }
         else if (!empty($_POST['com']) && !empty($_POST['subcom'])){
             $logUser = $_SESSION['login'];
             $comment = $_POST['com'];
             
             addDbCom($logUser, $comment, $file_img);
+            if ($addDbCom === 'error'){
+                $_SESSION['messInfo'] = 'Can\'t save comment.';
+            }
 
             if (empty($_SESSION['pref']) || $_SESSION['pref'] == 'yes'){
-                $comSent = sendCom($logUser);
-                if ($comSent == true){
-                    $_SESSION['messInfo'] = 'Mail sent';
-                }
-                else{
-                    $_SESSION['messInfo'] = 'Mail can\'t be sent.';
+                if (sendCom($logUser) === 'error'){
+                    $_SESSION['messInfo'] = 'Error.';
                 }
             }
             
             $comments = getComment($file_img);
+            if ($comments === 'error'){
+                $_SESSION['messInfo'] = 'Can\'t get picture\'s comments.';
+            }
         }
         else if (!empty($_POST['supp'])){
-            suppPhoto($file_img);
+            $supp = suppPhoto($file_img);
+            if ($supp === 'error'){
+                $_SESSION['messInfo'] = 'Error.';
+            }
+            else if($supp === true){
+                $_SESSION['messInfo'] = 'Picture deleted.';
+                header('Location: ' . $fullDomain . '/gallery');
+                exit;
+            }
         }
         header('Location: ' . $fullDomain . '/photo?log=' . $_POST['logUser'] . '&n=' . $idPhoto);
     }
 }
 
+else{
+    echo '404 Error';
+}
 ?>
--- models/photo.php
+++ models/photo.php
@@ -2,84 +2,135 @@
 include($root . '/config/database.php');
 
 function getPhoto($id_user, $id_photo){
-    $conn = connexion();
-    $sql = "SELECT `path` FROM `gallery` WHERE `id_user` = '{$id_user}' AND `id` = '{$id_photo}'";
-    $req = $conn->query($sql);
-    $conn = null;
-    $data = $req->fetchAll(PDO::FETCH_ASSOC);
-    return $data[0];
+    try{
+        $conn = connexion();
+        $sql = "SELECT `path` FROM `gallery` WHERE `id_user` = '{$id_user}' AND `id` = '{$id_photo}'";
+        $req = $conn->query($sql);
+        $conn = null;
+        $data = $req->fetchAll(PDO::FETCH_ASSOC);
+        return $data[0];
+    }
+    catch(PDOException $err){
+        return 'error';
+    }
 }
 
 function getLogin($id){
-    $conn = connexion();
-    $sql = "SELECT `login` FROM `user` WHERE `id` = '{$id}';";
-    $req = $conn->query($sql);
-    $conn = null;
-    $data = $req->fetchAll(PDO::FETCH_ASSOC);
-    return $data[0];
+    try{
+        $conn = connexion();
+        $sql = "SELECT `login` FROM `user` WHERE `id` = '{$id}';";
+        $req = $conn->query($sql);
+        $conn = null;
+        $data = $req->fetchAll(PDO::FETCH_ASSOC);
+        return $data[0];
+    }
+    catch(PDOException $err){
+        return 'error';
+    }
 }
 
 function getId($login){
-    $conn = connexion();
-    $sql = "SELECT `id` FROM `user` WHERE `login` = '{$login}'";
-    $req = $conn->query($sql);
-    $conn = null;
-    $data = $req->fetchAll(PDO::FETCH_ASSOC);
-    return $data[0];
+    try{
+        $conn = connexion();
+        $sql = "SELECT `id` FROM `user` WHERE `login` = '{$login}'";
+        $req = $conn->query($sql);
+        $conn = null;
+        $data = $req->fetchAll(PDO::FETCH_ASSOC);
+        return $data[0];
+    }
+    catch(PDOException $err){
+        return 'error';
+    }
 }
 
 function getMail($login){
-    $conn = connexion();
-    $sql = "SELECT `mail` FROM `user` WHERE `login` = '{$login}';";
-    $req = $conn->query($sql);
-    $conn = null;
-    $data = $req->fetchAll(PDO::FETCH_ASSOC);
-    return $data[0]['mail'];
+    try{
+        $conn = connexion();
+        $sql = "SELECT `mail` FROM `user` WHERE `login` = '{$login}';";
+        $req = $conn->query($sql);
+        $conn = null;
+        $data = $req->fetchAll(PDO::FETCH_ASSOC);
+        return $data[0]['mail'];
+    }
+    catch(PDOException $err){
+        return 'error';
+    }
 }
 
 function countLike($id_user, $id_photo){
-    $conn = connexion();
-    $sql = "SELECT `nb_like` FROM `gallery` WHERE `id_user` = '{$id_user}' AND `id` = '{$id_photo}'";
-    $req = $conn->query($sql);
-    $conn = null;
-    $data = $req->fetchAll(PDO::FETCH_ASSOC);
-    return $data[0];
+    try{
+        $conn = connexion();
+        $sql = "SELECT `nb_like` FROM `gallery` WHERE `id_user` = '{$id_user}' AND `id` = '{$id_photo}'";
+        $req = $conn->query($sql);
+        $conn = null;
+        $data = $req->fetchAll(PDO::FETCH_ASSOC);
+        return $data[0];
+    }
+    catch(PDOException $err){
+        return 'error';
+    }
 }
 
 function likePhoto($namePhoto){
-    $conn = connexion();
-    $sql = "UPDATE `gallery` SET `nb_like` = `nb_like` + 1 WHERE `path` = '{$namePhoto}';";
-    $conn->query($sql);
-    $conn = null;
+    try{
+        $conn = connexion();
+        $sql = "UPDATE `gallery` SET `nb_like` = `nb_like` + 1 WHERE `path` = '{$namePhoto}';";
+        $conn->query($sql);
+        $conn = null;
+        return true;
+    }
+    catch(PDOException $err){
+        return 'error';
+    }
 }
 
 function addDbCom($logUser, $comment, $filename){
-    $conn = connexion();
-    $idUser = getId($logUser);
-    $sql = "INSERT INTO `com` (`id_user`, `comment`, `picname`) VALUES ('{$idUser['id']}', '{$comment}', '{$filename}');";
-    $conn->query($sql);
-    $conn = null;
+    try{
+        $conn = connexion();
+        $idUser = getId($logUser);
+        $sql = "INSERT INTO `com` (`id_user`, `comment`, `picname`) VALUES ('{$idUser['id']}', '{$comment}', '{$filename}');";
+        $conn->query($sql);
+        $conn = null;
+        return true;
+    }
+    catch(PDOException $err){
+        return 'error';
+    }
 }
 
 function getComment($filename){
-    $conn = connexion();
-    $sql = "SELECT * FROM `com` WHERE `picname` = '{$filename}';";
-    $req = $conn->query($sql);
-    $conn = null;
-    $data = $req->fetchAll(PDO::FETCH_ASSOC);
-    return $data;
+    try{
+        $conn = connexion();
+        $sql = "SELECT * FROM `com` WHERE `picname` = '{$filename}';";
+        $req = $conn->query($sql);
+        $conn = null;
+        $data = $req->fetchAll(PDO::FETCH_ASSOC);
+        return $data;
+    }
+    catch(PDOException $err){
+        return 'error';
+    }
 }
 
 function suppPhoto($name){
-    $conn = connexion();
-    $sql = "DELETE FROM `gallery` WHERE `path` = '{$name}';";
-    $sql .= "DELETE FROM `com` WHERE `picname` = '{$name}';";
-    $conn->query($sql);
-    $conn = null;
+    try{
+        $conn = connexion();
+        $sql = "DELETE FROM `gallery` WHERE `path` = '{$name}';";
+        $sql .= "DELETE FROM `com` WHERE `picname` = '{$name}';";
+        $conn->query($sql);
+        $conn = null;
+        return true;
+    }
+    catch(PDOException $err){
+        return 'error';
+    }
 }
 
 function sendCom($login){
     $mail = getMail($login);
+    if ($mail === 'error'){
+        return 'error';
+    }
     $subject = "Picture's comment";
     $headers = "MIME-Version: 1.0" . "\r\n";
     $headers = "Content-type:text/html;charset=UTF-8" . "\r\n";
@@ -93,16 +144,21 @@ function sendCom($login){
 }
 
 function photoExists($idUser, $idPhoto){
-    $conn = connexion();
-    $sql = "SELECT COUNT(*) AS 'count' FROM `gallery` WHERE `id_user` = '{$idUser}' AND `id` = '{$idPhoto}';";
-    $req = $conn->query($sql);
-    $conn = null;
-    $data = $req->fetchAll(PDO::FETCH_ASSOC);
-    if ($data[0]['count'] > 0){
-        return true;
+    try{
+        $conn = connexion();
+        $sql = "SELECT COUNT(*) AS 'count' FROM `gallery` WHERE `id_user` = '{$idUser}' AND `id` = '{$idPhoto}';";
+        $req = $conn->query($sql);
+        $conn = null;
+        $data = $req->fetchAll(PDO::FETCH_ASSOC);
+        if ($data[0]['count'] > 0){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
-    else{
-        return false;
+    catch(PDOException $err){
+        return 'error';
     }
 }
 
