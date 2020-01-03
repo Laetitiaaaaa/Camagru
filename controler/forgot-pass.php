53ecb0063703fec8355819f6e43687ec598122bc
Laetitia LEJEUNE <llejeune@e3r5p17.42.fr>
2020-01-03 15:40:31 +0100

    forgotPass try catch ok

--- controler/forgot-pass.php
+++ controler/forgot-pass.php
@@ -5,18 +5,29 @@ if ($method == 'GET'){
     require($root . '/views/forgot-pass.php');
 }
 
-if ($method == 'POST'){
+else if ($method == 'POST'){
     if (!empty($_POST['mail'])){
         $mail = $_POST['mail'];
         $change = sendPass($mail);
-        if ($change == true){
+
+        if ($change === true){
             $_SESSION['messInfo'] = 'Mail has been successfully sent!';
         }
-        else{
+        else if ($change === false){
             $_SESSION['messInfo'] = "Mail can't be sent.";
+        }
+        else if ($change === 'error mail'){
+            $_SESSION['messInfo'] = 'Mail already exists.';
+        }
+        else{
+            $_SESSION['messInfo'] = 'Error.';
         }
     }
     header('Location: ' . $fullDomain . '/forgot-pass');
+}
+
+else{
+    echo '404 Error';
 }
 
 ?>
--- models/forgot-pass.php
+++ models/forgot-pass.php
@@ -2,56 +2,78 @@
 include($root . '/config/database.php');
 
 function isMail($mail){
-    $conn = connexion();
-    $sql = "SELECT `mail` FROM `user` WHERE `mail` = '{$mail}';";
-    $req = $conn->query($sql);
-    $data = $req->fetchAll(PDO::FETCH_ASSOC);
-    $conn = null;
-    if ($data){
-        return true;
+    try{
+        $conn = connexion();
+        $sql = "SELECT `mail` FROM `user` WHERE `mail` = '{$mail}';";
+        $req = $conn->query($sql);
+        $data = $req->fetchAll(PDO::FETCH_ASSOC);
+        $conn = null;
+        if ($data){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
-    else{
-        return false;
+    catch(PDOException $err){
+        return 'error';
     }
 }
 
 function getUserwMail($mail){
-    $conn = connexion();
-    $sql = "SELECT * FROM `user` WHERE `mail` = '{$mail}';";
-    $req = $conn->query($sql);
-    $data = $req->fetchAll(PDO::FETCH_ASSOC);
-    $conn = null;
-    return ($data[0]);
+    try{
+        $conn = connexion();
+        $sql = "SELECT * FROM `user` WHERE `mail` = '{$mail}';";
+        $req = $conn->query($sql);
+        $data = $req->fetchAll(PDO::FETCH_ASSOC);
+        $conn = null;
+        return ($data[0]);
+    }
+    catch(PDOException $err){
+        return 'error';
+    }
 }
 
 function insertNum($mail, $num){
-    $conn = connexion();
-    $sql = "UPDATE `user` SET `num` = '{$num}' WHERE `mail` = '{$mail}';";
-    $conn->query($sql);
-    $conn = null;
-    var_dump("num user inserted");
+    try{
+        $conn = connexion();
+        $sql = "UPDATE `user` SET `num` = '{$num}' WHERE `mail` = '{$mail}';";
+        $conn->query($sql);
+        $conn = null;
+        return true;
+    }
+    catch(PDOException $err){
+        return 'error';
+    }
 }
 
 function sendPass($mail){
-    if (isMail($mail) == false){
-        var_dump("Error: mail doesn't exist.");
+    $check = isMail($mail);
+    if ($check === false){
+        return 'error mail';
     }
-    else{
-        $num = rand(0, 1000000);
-        insertNum($mail, $num);
-        $user = getUserwMail($mail);
-        $login = $user['login'];
-        $subject = "Forgotten password";
-        $headers = "MIME-Version: 1.0" . "\r\n";
-        $headers = "Content-type:text/html;charset=UTF-8" . "\r\n";
-        $headers .= "From: Camagru Team <no_reply@camagru.com>"."\r\n";
-        $message = "
-        <h1>No worries $login!</h1>
-        <p>To change your password, click <a href='http://localhost:8080/change-pass?log=$login&n=$num'>here</a>.</p>
-        ";
-        $chpass = mail($mail, $subject, $message, $headers);
-        return $chpass;
+    else if ($check === 'error'){
+        return 'error';
     }
+    $num = rand(0, 1000000);
+    if (insertNum($mail, $num) === 'error'){
+        return 'error';
+    }
+    $user = getUserwMail($mail);
+    if ($user === 'error'){
+        return 'error';
+    }
+    $login = $user['login'];
+    $subject = "Forgotten password";
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers = "Content-type:text/html;charset=UTF-8" . "\r\n";
+    $headers .= "From: Camagru Team <no_reply@camagru.com>"."\r\n";
+    $message = "
+    <h1>No worries $login!</h1>
+    <p>To change your password, click <a href='http://localhost:8080/change-pass?log=$login&n=$num'>here</a>.</p>
+    ";
+    $chpass = mail($mail, $subject, $message, $headers);
+    return $chpass;
 }
 
 ?>
