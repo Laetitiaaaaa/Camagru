79ae5c2ac575aa215b6d58b6bb7e9912348fc889
Laetitia LEJEUNE <llejeune@e3r5p17.42.fr>
2020-01-03 20:18:33 +0100

    css forgotten password ok

--- views/forgot-pass.php
+++ views/forgot-pass.php
@@ -1,5 +1,59 @@
 <?php include('header.php');?>
 
+<section class="section">
+
+<div class="columns">
+<div class="column is-3"></div>
+<div class="column is-6">
+
+    <div class="container">
+
+    <section class="hero is-dark">
+        <div class="hero-body">
+            <div class="container">
+                <h1 class="title">
+                    Forgotten password
+                </h1>  
+            </div>
+        </div>
+    </section>
+
+    <div class="notification">
+        <form name="forgotPass" method="post" action="/forgot-pass">
+            <div class="field">
+                <p class="control has-icons-left has-icons-right">
+                    <input name="mail" class="input" type="email" placeholder="Mail address"/>
+                    <span class="icon is-small is-left">
+                        <i class="fas fa-envelope"></i>
+                    </span>
+                </p>
+            </div>
+            
+            <div class="field">
+                <p class="control">
+                    <input name="submit" type="submit" value="Send">
+                </p>
+            </div>
+        </form>
+    
+    </div>
+
+    </div>
+
+</div>
+<div class="column is-3"></div>
+</div>
+
+</section>
+
+
+
+
+
+
+
+
+<!-- 
 <h2>Forgotten password</h2>
 
 <form name="forgotPass" method="post" action="/forgot-pass">
@@ -7,6 +61,6 @@
 <input name="mail" type="email" placeholder="Mail address"/></br> 
 <input name="submit" type="submit" value="Send"></br>
 
-</form>
+</form> -->
 
 <?php include('footer.php'); ?>
