aeefa08bc4267d506abd738f96e78563a3b2c2f1
Laetitia LEJEUNE <llejeune@e3r5p17.42.fr>
2020-01-03 14:29:23 +0100

    my account try catch ok

--- controler/myAccount.php
+++ controler/myAccount.php
@@ -5,24 +5,54 @@ if ($method == 'GET'){
     require($root . '/views/myAccount.php');
 }
 
-if ($method == 'POST'){
+else if ($method == 'POST'){
     if (!empty($_POST['login']) && !empty($_POST['changeLog'])){
         $login = $_POST['login'];
-        changeLog($_SESSION['login'], $login);
-        $_SESSION['login'] = $login;
-        $_SESSION['messInfo'] = 'Your loggin has been successfully changed.';
+        $check = changeLog($_SESSION['login'], $login);
+
+        if ($check === true){
+            $_SESSION['login'] = $login;
+            $_SESSION['messInfo'] = 'Your loggin has been successfully changed.';
+        }
+        else if($check === false){
+            $_SESSION['messInfo'] = 'Login already exists.';
+        }
+        else{
+            $_SESSION['messInfo'] = 'Error.';
+        }
     }
     else if (!empty($_POST['mail']) && !empty($_POST['changeMail'])){
         $mail = $_POST['mail'];
-        changeMail($_SESSION['login'], $mail);
-        $_SESSION['mail'] = $mail;
-        $_SESSION['messInfo'] = 'Your mail address has been successfully changed.';
+        $check = changeMail($_SESSION['login'], $mail);
+
+        if ($check === true){
+            $_SESSION['mail'] = $mail;
+            $_SESSION['messInfo'] = 'Your mail address has been successfully changed.';
+        }
+        else if ($check === false){
+            $_SESSION['messInfo'] = 'Mail already exists.';
+        }
+        else{
+            $_SESSION['messInfo'] = 'Error.';
+        }
     }
     else if (!empty($_POST['password']) && !empty($_POST['verif']) && !empty($_POST['changePass'])){
         $newPass = $_POST['password'];
         $verif = $_POST['verif'];
-        changePass($_SESSION['login'], $newPass, $verif);
-        $_SESSION['messInfo'] = 'Your password has been successfully changed.';
+        $check = changePass($_SESSION['login'], $newPass, $verif);
+
+        if($check === true){
+            $_SESSION['messInfo'] = 'Your password has been successfully changed.';
+        }
+        else if ($check === 'verif wrong'){
+            $_SESSION['messInfo'] = 'You must write the same password twice.';
+        }
+        else if ($check === 'pass wrong'){
+            $_SESSION['messInfo'] = 'Password not well formated; Password need at least 8 characters, one lowercase and one uppercase.';
+        }
+        else{
+            $_SESSION['messInfo'] = 'Error.';
+        }
     }
     else if (!empty($_POST['preference']) && !empty($_POST['pref'])){
         $_SESSION['pref'] = $_POST['preference'];
@@ -30,6 +60,10 @@ if ($method == 'POST'){
     }
     header('Location: ' . $fullDomain . '/my-account');
     exit;
+}
+
+else{
+    echo '404 Error';
 }
 
 ?>
--- models/myAccount.php
+++ models/myAccount.php
@@ -2,30 +2,40 @@
 include($root . '/config/database.php');
 
 function isLogin($login){
-    $conn = connexion();
-    $sql = "SELECT `login` FROM `user_sub` WHERE `login` = '{$login}' UNION SELECT `login` FROM `user` WHERE `login` = '{$login}';";
-    $req = $conn->query($sql);
-    $data = $req->fetchAll(PDO::FETCH_ASSOC);
-    $conn = null;
-    if ($data){
-        return true;
+    try{
+        $conn = connexion();
+        $sql = "SELECT `login` FROM `user_sub` WHERE `login` = '{$login}' UNION SELECT `login` FROM `user` WHERE `login` = '{$login}';";
+        $req = $conn->query($sql);
+        $data = $req->fetchAll(PDO::FETCH_ASSOC);
+        $conn = null;
+        if ($data){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
-    else{
-        return false;
+    catch(PDOException $err){
+        return 'error';
     }
 }
 
 function isMail($mail){
-    $conn = connexion();
-    $sql = "SELECT `mail` FROM `user_sub` WHERE `mail` = '{$mail}' UNION SELECT `mail` FROM `user` WHERE `mail` = '{$mail}';";
-    $req = $conn->query($sql);
-    $data = $req->fetchAll(PDO::FETCH_ASSOC);
-    $conn = null;
-    if ($data){
-        return true;
+    try{
+        $conn = connexion();
+        $sql = "SELECT `mail` FROM `user_sub` WHERE `mail` = '{$mail}' UNION SELECT `mail` FROM `user` WHERE `mail` = '{$mail}';";
+        $req = $conn->query($sql);
+        $data = $req->fetchAll(PDO::FETCH_ASSOC);
+        $conn = null;
+        if ($data){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
-    else{
-        return false;
+    catch(PDOException $err){
+        return 'error';
     }
 }
 
@@ -39,21 +49,10 @@ function checkPasswd($password){
                 if (strlen($password) > 7){
                     return true;
                 }
-                else{
-                    return false;
-                }
-            }
-            else{
-                return false;
             }
         }
-        else{
-            return false;
-        }
     }
-    else{
-        return false;
-    }
+    return false;
 }
 
 function matchPassVerif($passwd, $verif){
@@ -64,44 +63,70 @@ function matchPassVerif($passwd, $verif){
 }
 
 function changeLog($oldLog, $newLog){
-    if (isLogin($newLog) == false){
-        $conn = connexion();
-        $sql = "UPDATE `user` SET `login` = '{$newLog}' WHERE `login` = '{$oldLog}';";
-        $conn->query($sql);
-        $conn = null;
+    $log = isLogin($newLog);
+    if ($log === false){
+        try{
+            $conn = connexion();
+            $sql = "UPDATE `user` SET `login` = '{$newLog}' WHERE `login` = '{$oldLog}';";
+            $conn->query($sql);
+            $conn = null;
+            return true;
+        }
+        catch(PDOException $err){
+            return 'error';
+        }
     }
     else{
-        var_dump('log already exists');
+        if ($log === 'error'){
+            return 'error';
+        }
+        return false;
     }
 }
 
 function changeMail($login, $newMail){
-    if (isMail($newMail) == false){
-        $conn = connexion();
-        $sql = "UPDATE `user` SET `mail` = '{$newMail}' WHERE `login` = '{$login}';";
-        $conn->query($sql);
-        $conn = null;
+    $mail = isMail($newMail);
+    if ($mail === false){
+        try{
+            $conn = connexion();
+            $sql = "UPDATE `user` SET `mail` = '{$newMail}' WHERE `login` = '{$login}';";
+            $conn->query($sql);
+            $conn = null;
+            return true;
+        }
+        catch(PDOException $err){
+            return 'error';
+        }
     }
     else{
-        var_dump('mail already exists');
+        if ($mail === 'error'){
+            return 'error';
+        }
+        return false;
     }
 }
 
 function changePass($login, $newPass, $verif){
     if (checkPasswd($newPass) == true){
         if (matchPassVerif($newPass, $verif) == true){
-            $newPass = hash('whirlpool', $newPass);
-            $conn = connexion();
-            $sql = "UPDATE `user` SET `password` = '{$newPass}' WHERE `login` = '{$login}';";
-            $conn->query($sql);
-            $conn = null;
+            try{
+                $newPass = hash('whirlpool', $newPass);
+                $conn = connexion();
+                $sql = "UPDATE `user` SET `password` = '{$newPass}' WHERE `login` = '{$login}';";
+                $conn->query($sql);
+                $conn = null;
+                return true;              
+            }
+            catch(PDOException $err){
+                return 'error';
+            }
         }
         else{
-            var_dump('you must right the same password as above');
+            return 'verif wrong';
         }
     }
     else{
-        var_dump('passwd must contain minuscule majuscule and number and be at least 8 characters long');
+        return 'pass wrong';
     }
 }
 ?>
