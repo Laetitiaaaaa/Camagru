9eee8d7b30ed4e5b3f749fb7d47093237ecf0f30
Laetitia LEJEUNE <llejeune@e3r5p19.42.fr>
2020-01-04 14:35:30 +0100

    css photo ok

--- views/notLogon.php
+++
@@ -1,5 +0,0 @@
-<?php include('header.php');?>
-
-<p>You must <a href="/controler/signIn.php">sign in</a> first.</p>
-
-<?php include('footer.php'); ?>
--- views/photo.php
+++ views/photo.php
@@ -1,26 +1,60 @@
 <?php include('header.php');?>
 
-<div>
+<div class="columns">
+<div class="column is-2"></div>
+<div class="column is-8">
+
+<div class="columns">
+<div class="column is-1"></div>
+<div class="column is-10" style="text-align: center;">
 <img src="/gallery/newPictures/<?php echo $file_img; ?>" alt="photo">
-<p><?php echo $nbLike['nb_like'] ?> like<?php if ($nbLike['nb_like'] > 1){ ?>s<?php } ?></p>
 </div>
+<div class="column is-1"></div>
+</div>
+
 <form method="post" action="/photo?log=<?php echo $_GET['log'] ?>&n=<?php echo $id_photo ?>">
     <input type="hidden" name="logUser" value="<?php echo $_GET['log'] ?>">
     <input type="hidden" name="idPhoto" value="<?php echo $id_photo ?>">
     <input type="hidden" name="namePhoto" value="<?php echo $file_img ?>">
-    <input type="submit" name="like" value="Like">
+
+    <div class="buttons is-centered">
+    <input type="submit" class="button is-danger is-outlined is-rounded" name="like" value="<?php echo $nbLike['nb_like'] ?> like<?php if ($nbLike['nb_like'] > 1){ ?>s<?php } ?>">
+    
     <?php if (getId($_SESSION['login']) == $id_user) {?>
-    <input type="submit" name="supp" value="Delete picture">
+    <input type="submit" class="button is-outligned is-rounded" name="supp" value="Delete picture">
     <?php } ?>
-    </br>
-    <div>
+    </div>
+    
+    
+    
     
     <?php foreach ($comments as $com) {$login = getLogin($com['id_user']);?>
-    <p><font color="purple"> <?php echo $login['login'] ?>:</font> <?php echo $com['comment']; ?></p>
+        <article class="message is-primary">
+        <div class="message-header">
+            <p><?php echo $login['login'] ?></p>
+        </div>
+        <div class="message-body">
+            <?php echo $com['comment']; ?>
+        </div>
+        </article>
+
     <?php } ?>
     
+
+
+
+    <div class="field">
+        <div class="control">
+        <input type="text" class="input is-primary" name="com" placeholder="leave a comment">
+        </div>
     </div>
-    <input type="text" name="com" placeholder="leave a comment">
-    <input type="submit" name="subcom" value="Submit">
+
+    <div class="buttons is-centered">
+        <input type="submit" class="button is-primary is-rounded" name="subcom" value="Submit">
+    </div>
+    
 </form>
+</div>
+<div class="column is-2"></div>
+</div>
 <?php include('footer.php'); ?>
